0a0a582b202e60f9986ee0aab2ff288bcfed37cd
CAJames1995 <illred@hotmail.com>
2023-04-20 22:22:28 +0000

    functional scrollbar?

--- App/views/home.py
+++ App/views/home.py
@@ -8,39 +8,45 @@ from.index import index_views
 
 from App.controllers import (
     get_admin, 
-    get_all_internship,
-    get_all_internship_json,
-    update_internship_name,
-    create_ship,
+    get_ship,
     get_all_ship,
+    get_all_ship_json,
+    create_ship,
+    update_ship_name,
 )
 
 home_views = Blueprint('home_views', __name__, template_folder='../templates')
+
+
 # Home Page
 #----------------------------------------------------------------------------
 @home_views.route('/home', methods=['GET'])
 @login_required
 def homepage():
-    # internships = get_all_internship()
+    chosen= -1
     ships = get_all_ship()
     id = current_user.get_id()#get current user id
     admin = get_admin(id)#get the admin object
     name = admin.name #get admin name
-    return render_template('home.html', admin = name, internships= ships)
+    return render_template('home.html', admin = name, scrollers= ships, chosen=chosen)
+
+@home_views.route('/home/<int:ship_id>', methods=['GET'])
+@login_required
+def homepage_withships(ship_id):
+    chosen= get_ship(ship_id)
+    if chosen== None:
+        chosen=-1
+    ships = get_all_ship()
+    id = current_user.get_id()#get current user id
+    admin = get_admin(id)#get the admin object
+    name = admin.name #get admin name
+    return render_template('home.html', admin = name, scrollers= ships, chosen=chosen)
+
 
 # current error 
 # File "/workspace/IntershipProject/App/controllers/Internship.py", line 10, in get_all_internship
 #     return Internship.query.all()
 # AttributeError: module 'App.models.Internship' has no attribute 'query'
-
-# Logout
-# --------------------------------------------------------------------------
-@home_views.route("/home/logout", methods=["POST"])
-@login_required
-def logout():
-    logout_user()
-    flash('Logged Out')
-    return redirect('/')
 
     
 @home_views.route('/home', methods=['POST'])
@@ -55,5 +61,26 @@ def make_internship():
     else:
         flash(f"Internship not created!")
     return redirect('/home')
+    
+@home_views.route('/update/<int:ship_id>', methods=['POST'])
+@login_required
+def namechange(ship_id):
+    ship= get_ship(ship_id)
+    data = request.form
+    ship = change_name(ship_id,data['name'])
+    if ship!= None:
+        flash(f"Internship {data['name']} changed!")
+    else:
+        flash(f"Name not changed!")
+    return redirect('/home/ship_id')
 
+
+# Logout
+# --------------------------------------------------------------------------
+@home_views.route("/home/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    flash('Logged Out')
+    return redirect('/')
 
