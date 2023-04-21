a0751a2ba45d87fb4e1a8ddc0c7950a54aa10e8d
elizabethhalls <elizabeth.halls@my.uwi.edu>
2023-04-21 19:21:41 +0000

    modified:   App/models/Attendants.py 	modified:   App/templates/login.html

--- App/models/Attendants.py
+++ App/models/Attendants.py
@@ -2,20 +2,21 @@ from App.database import db
 from App.models import Ship, Intern
 
 class Attendants(db.Model):
+
     ship_id = db.Column(db.Integer, db.ForeignKey('ship.id'), primary_key=True, nullable=False)
     intern_id = db.Column(db.Integer, db.ForeignKey('intern.school_id'), primary_key=True, nullable=False)
+    ship = db.relationship('Ship', backref= 'attendees', lazy='joined')
+    intern = db.relationship('Intern', backref= 'attendees', lazy='joined')
 #   intern = db.relationship('intern', backref=db.backref('attendees', lazy='joined'))
  
     def __init__(self, ship_id, intern_id):
         self.ship_id = ship_id
-        self.ship_id = intern_id
+        self.intern_id = intern_id
     
-
-
     def get_json(self):
         return {
-        "Internship ID": self.ship_id,
-        "Intern ID": self.intern_id,
+        "Internship ID": self.ship.id,
+        "Intern ID": self.intern.id,
         # "Internship Name": self.ship_id.name,
         # "Intern Name": self.intern.id.name
         }
